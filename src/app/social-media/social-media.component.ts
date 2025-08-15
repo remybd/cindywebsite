0f39774ba0db9c14025c04e258469e21e1dcdbc7
Rémy Beulé Dauzat <remy.b3d@gmail.com>
2025-08-15 19:15:16 +0200

    feat: home page and basic structure for social media

---
+++ src/app/social-media/social-media.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {SocialMediaDataMock} from "../datas/social-media.mock";
+
+
+@Component({
+  selector: 'app-social-media',
+  templateUrl: './social-media.component.html',
+  styleUrl: './social-media.component.css',
+  standalone: false
+})
+export class SocialMediaComponent {
+  socialMedias = Object.values(SocialMediaDataMock.socialMediaDic);
+  contentSocialMediaPath = SocialMediaDataMock.contentSocialMediaPath;
+
+}
